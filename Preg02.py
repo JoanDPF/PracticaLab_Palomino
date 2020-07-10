e9c93395b14283e4ee36807e6613159bd74551f4
Hamet Calderon <hametangello@gmail.com>
2020-07-09 20:07:53 -0500

    Pregunta 02 - Listo

---
+++ Preg02.py
@@ -0,0 +1,9 @@
+#Autor: Hamet Calderon Salas 
+
+print("Validadcion de Correo")
+correo = input("Ingrese Correo: ")
+if "@untels.edu.pe" in correo:
+	print("Si es correo de la UNTELS")
+else:
+	print("No es correo de la UNTELS")
+
